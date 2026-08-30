1b9aeabc676ba04beca82feddb5b4b3e43147c4a
br4pz <brunapreal@gmail.com>
2026-08-30 19:31:25 -0300

    check 3 tp0

--- client/src/client.c
+++ client/src/client.c
@@ -49,6 +49,11 @@ int main(void)
 	conexion = crear_conexion(ip, puerto);
 
 	// Enviamos al servidor el valor de CLAVE como mensaje
+	int cod=MENSAJE;
+	int tam = strlen(valor)+1;
+	send(conexion,&cod,sizeof(int),0);
+	send(conexion, &tam, sizeof(int), 0);  
+	send(conexion,valor,tam,0);
 
 	// Armamos y enviamos el paquete
 	paquete(conexion);
@@ -57,7 +62,7 @@ int main(void)
 
 	/*---------------------------------------------------PARTE 5-------------------------------------------------------------*/
 	// Proximamente
-	log_destroy(logger);
+
 }
 
 t_log* iniciar_logger(void)
@@ -89,10 +94,13 @@ void leer_consola(t_log* logger)
 
 	while (leido[0]!='\0'){
 		log_info(logger,leido);
+		free(leido);
 		leido = readline("> ");
 	}
 
 	// ¡No te olvides de liberar las lineas antes de regresar!
+
+	free(leido);
 }
 
 void paquete(int conexion)
@@ -103,13 +111,28 @@ void paquete(int conexion)
 
 	// Leemos y esta vez agregamos las lineas al paquete
 
+	paquete = crear_paquete();
+
+	leido = readline("> ");
+
+	while (leido[0]!='\0'){
+		agregar_a_paquete(paquete,leido,strlen(leido)+1);
+		free(leido);
+		leido = readline("> ");
+	}
+	printf("hasta aca llegamos");
+	enviar_paquete(paquete,conexion);
 
 	// ¡No te olvides de liberar las líneas y el paquete antes de regresar!
-	
+	free(leido);
+	eliminar_paquete(paquete);
+	//! liberamos primero el buffer, sino se genera memory leak
 }
 
 void terminar_programa(int conexion, t_log* logger, t_config* config)
-{
+{	
+	log_destroy(logger);
+	config_destroy(config);
 	/* Y por ultimo, hay que liberar lo que utilizamos (conexion, log y config) 
 	  con las funciones de las commons y del TP mencionadas en el enunciado */
 }
--- client/src/utils.c
+++ client/src/utils.c
@@ -29,9 +29,14 @@ int crear_conexion(char *ip, char* puerto)
 	getaddrinfo(ip, puerto, &hints, &server_info);
 
 	// Ahora vamos a crear el socket.
-	int socket_cliente = 0;
+	int socket_cliente = socket(server_info->ai_family,
+								server_info->ai_socktype,
+								server_info->ai_protocol
+							);
 
 	// Ahora que tenemos el socket, vamos a conectarlo
+    int resultado = connect(socket_cliente,server_info->ai_addr,server_info->ai_addrlen);
+    printf("connect devolvio: %d\n", resultado);
 
 
 	freeaddrinfo(server_info);
@@ -89,7 +94,6 @@ void enviar_paquete(t_paquete* paquete, int socket_cliente)
 {
 	int bytes = paquete->buffer->size + 2*sizeof(int);
 	void* a_enviar = serializar_paquete(paquete, bytes);
-
 	send(socket_cliente, a_enviar, bytes, 0);
 
 	free(a_enviar);
